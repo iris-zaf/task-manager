a7b79044081775381f87662396f400f7a2fad942
Iris <iriskalogirou1@gmail.com>
2024-05-19 19:15:56 +0300

    fix form view and add alpine

--- taskmanager/resources/views/form.blade.php
+++ taskmanager/resources/views/form.blade.php
@@ -2,12 +2,7 @@
 
 @section('title', isset($task)?"Edit Task": "Add Task")
 @section('styles')
-<style>
-.error-message {
-    color: red;
-    font-size: 0.8rem;
-}
-</style>
+
 @endsection
 
 @section('content')
@@ -18,43 +13,45 @@
     @isset($task)
     @method('PUT')
     @endisset
-    <div>
+    <div class="mb-4">
         <label for="title">
             Title
         </label>
-        <input type="text" name="title" id="title" value="{{ $task->title ?? old('title') }}" />
+        <input type="text" name="title" id="title" @class(['border-red-500'=>$errors->has('title')])
+        value="{{ $task->title ?? old('title') }}" />
         @error('title')
-        <p class="error-message">*{{ $message }}</p>
+        <p class="error">*{{ $message }}</p>
         @enderror
     </div>
-    <div>
+    <div class="mb-4">
         <label for="description">
             Description
         </label>
-        <textarea type="text" name="description" id="description"
+        <textarea type="text" name="description" id="description" @class(['border-red-500'=>$errors->has('description')])
             rows="5">{{ $task->description ?? old('description') }}</textarea>
 
         @error('description')
-        <p class="error-message">*{{ $message }}</p>
+        <p class="error">*{{ $message }}</p>
         @enderror
     </div>
-    <div>
+    <div class="mb-4">
         <label for="long_description">
             Long Description
         </label>
-        <textarea type="text" name="long_description" id="long_description"
+        <textarea type="text" name="long_description" id="long_description" @class(['border-red-500'=>$errors->has('long_description')])
             rows="10">{{  $task->long_description ?? old('long_description')}}</textarea>
         @error('long_description')
-        <p class="error-message">*{{ $message }}</p>
+        <p class="error">*{{ $message }}</p>
         @enderror
     </div>
-    <div>
-        <button type="submit">@isset($task)
+    <div class="flex gap-2 items-center">
+        <button type="submit" class="btn">@isset($task)
             Update task
             @else
             Add Task
             @endisset
         </button>
+        <a href="{{ route('tasks.index')}}" class="link">Cancel</a>
     </div>
 </form>
 @endsection
--- taskmanager/resources/views/index.blade.php
+++ taskmanager/resources/views/index.blade.php
@@ -3,19 +3,20 @@
 @section('title','List of Tasks')
 
 @section('content')
-<div>
-    <a href="{{route('tasks.create')}}">Add Task</a>
-</div>
+<nav class="mb-4">
+    <a href="{{route('tasks.create')}}" class="link">Add Task</a>
+</nav>
 @forelse($tasks as $task)
 <div>
-    <a href="{{ route('tasks.show',['task' =>$task->id])}}">{{ $task ->title}}</a>
+    <a href="{{ route('tasks.show',['task' =>$task->id])}}" @class(['line-through'=>
+        $task->completed])>{{ $task ->title}}</a>
 </div>
 @empty
 <div>There are tasks!</div>
 @endforelse
 @if ($tasks->count())
-
-{{$tasks->links()}}
-
+<nav class="mt-4">
+    {{$tasks->links()}}
+</nav>
 @endif
 @endsection
